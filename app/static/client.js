04f43c8e41380b962e411c571aee524c9f519ccc
sacros12 <102010094+sacros12@users.noreply.github.com>
2022-05-16 11:12:18 +0530

    Create client.js

---
+++ app/static/client.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#selected_image')
+                    .attr('src', e.target.result)
+                    .width(176)
+                    .height(176);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $('#imagefile').change(function () {
+        readURL(this);
+    });
+
+
+    $("form#analysis-form").submit(function (event) {
+        event.preventDefault();
+
+        var analyze_button = $("button#analyze-button");
+        var imagefile = $('#imagefile')[0].files;
+
+        if (!imagefile.length > 0) {
+            alert("Please select a file to analyze!");
+        }
+        else {
+            $("#analyze-button").html("Analyzing..");
+            $("#analyze-button").prop("disabled", "true");
+
+            var fd = new FormData();
+            fd.append('file', imagefile[0]);
+
+            var loc = window.location;
+
+            $.ajax({
+                method: 'POST',
+                async: true,
+                url: loc.protocol + '//' + loc.hostname + ':' + loc.port + '/analyze',
+                data: fd,
+                processData: false,
+                contentType: false,
+            }).done(function (data) {
+                console.log("Done Request!");
+                $("#result").html(data.result);
+                $("#description").html(data.description);
+                $("#prevention").html(data.prevention);
+            }).fail(function (e) {
+                console.log("Fail Request!");
+                console.log(e);
+            });
+        };
+
+        $("#analyze-button").prop("disabled", "");
+        $("#analyze-button").html("Analyze");
+        console.log("Submitted!");
+    });
+});
